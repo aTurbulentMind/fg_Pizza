ffff60b0f08d7608b638b1698816f52c6a143852
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2023-12-07 14:56:30 -0800

    Add files via upload

---
+++ build/_app/immutable/chunks/mushIngr.63302b3b.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/mushIngr.7b459a5d.jpg",import.meta.url).href;export{e as p};
---
+++ build/_app/immutable/nodes/3.6d872335.js
@@ -0,0 +1,22 @@
+import{s as b,n as c}from"../chunks/scheduler.e108d1fd.js";import{S as w,i as _,g as h,s as f,h as d,x as u,c as v,k as y,a as i,f as n}from"../chunks/index.ac40e99e.js";import{p as x}from"../chunks/mushIngr.63302b3b.js";const k=""+new URL("../assets/steamyComboPull.8ab57200.jpg",import.meta.url).href,T=""+new URL("../assets/Aboutim.50eb61d0.jpg",import.meta.url).href;function P(z){let t,m=`<img src="${T}" alt="background"/>`,r,a,p="About",l,o,g=`<p class="text_Box">Fairgrounds Pizza unfolds a remarkable five-decade tale, woven with passion, tradition, and a
+		commitment to serving the community. In 1973, our founders, Maria and Tony, embarked on a
+		culinary journey, transforming a modest corner into what would become the heart of the
+		neighborhood. From the very beginning, their dedication to crafting exceptional pizzas and
+		fostering a welcoming atmosphere laid the foundation for Fairgrounds Pizza&#39;s enduring legacy.</p> <p class="text_Box glass_Back">As the years unfolded, Fairgrounds Pizza became a cherished gathering place for families,
+		friends, and pizza enthusiasts alike. The aroma of our freshly baked pies became synonymous with
+		the warmth and camaraderie that filled the air. Through the ups and downs of the decades, the
+		pizzeria evolved, adapting to changing tastes while staying true to its roots.</p> <p class="text_Box">In the midst of the Great Robot War of 1993, Fairgrounds Pizza stood resilient, a beacon of
+		normalcy in tumultuous times. Despite the challenges posed by the robotic uprising, our
+		dedicated team continued to roll out dough, spread sauce, and sprinkle toppings, offering a
+		haven where the community could find solace and comfort. The pizzeria became a symbol of
+		perseverance, turning each pizza into a testament of resilience and a slice of hope during those
+		challenging days.</p> <div class="img_Gap svelte-wo1u47"><img src="${x}" alt="background" class="svelte-wo1u47"/></div> <p class="text_Box glass_Back">Throughout the fifty-year journey, Fairgrounds Pizza has been more than a place to enjoy
+		delicious food; it&#39;s a living testament to the power of community and the enduring appeal of a
+		slice of the past. From the original recipes passed down through generations to the smiling
+		faces that have walked through our doors, each element contributes to the rich tapestry that is
+		the story of Fairgrounds Pizza. As we celebrate this half-century milestone, we look back with
+		gratitude for the memories shared, the challenges overcome, and the joy found in every
+		pizza-loving moment.</p> <p class="text_Box">Today, as we continue our tradition of serving mouthwatering pizzas, Fairgrounds Pizza remains a
+		cornerstone of the neighborhood, a place where families create new memories and friends come
+		together over a shared love for exceptional pizza. Here&#39;s to the next fifty years of
+		deliciousness, community, and the timeless joy of Fairgrounds Pizza.</p> <div class="img_Gap svelte-wo1u47"><img src="${k}" alt="background" class="svelte-wo1u47"/></div>`;return{c(){t=h("banner"),t.innerHTML=m,r=f(),a=h("destination"),a.textContent=p,l=f(),o=h("div"),o.innerHTML=g,this.h()},l(e){t=d(e,"BANNER",{"data-svelte-h":!0}),u(t)!=="svelte-1ch9k8k"&&(t.innerHTML=m),r=v(e),a=d(e,"DESTINATION",{class:!0,"data-svelte-h":!0}),u(a)!=="svelte-1nxpxwx"&&(a.textContent=p),l=v(e),o=d(e,"DIV",{class:!0,"data-svelte-h":!0}),u(o)!=="svelte-87t96g"&&(o.innerHTML=g),this.h()},h(){y(a,"class","svelte-wo1u47"),y(o,"class","text_Block")},m(e,s){i(e,t,s),i(e,r,s),i(e,a,s),i(e,l,s),i(e,o,s)},p:c,i:c,o:c,d(e){e&&(n(t),n(r),n(a),n(l),n(o))}}}class C extends w{constructor(t){super(),_(this,t,null,P,b,{})}}export{C as component};
